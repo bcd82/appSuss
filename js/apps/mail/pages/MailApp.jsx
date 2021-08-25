b62bc15355b6f0c46306bf7042826b59942373e3
bcd82 <barak.sidi@gmail.com>
2021-08-25 12:42:25 +0300

    enabled routes to mail pages and added functions to page components

---
+++ js/apps/mail/pages/MailApp.jsx
@@ -0,0 +1,10 @@
+
+export class MailApp extends React.Component {
+    render() {
+        return (
+            <section className="mail-app">
+                <h1>Hey there from Mail</h1>
+            </section>
+        )
+    }
+}
---
+++ js/apps/mail/pages/MailDetails.jsx
@@ -0,0 +1,10 @@
+
+export  class MailDetails extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hi from  Mail Details Page</h1>
+            </div>
+        )
+    }
+}
