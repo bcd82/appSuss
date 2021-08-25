4830acdc46b352e8c1f7139921d6665700d7ee4a
ronbuchris <ronbuchris@gmail.com>
2021-08-25 11:05:01 +0300

    header jsx build

---
+++ js/cmps/AppHeader.jsx
@@ -0,0 +1,21 @@
+const { NavLink, withRouter } = ReactRouterDOM;
+
+class _AppHeader extends React.Component {
+  render() {
+    return (
+      <section className='app-header'>
+        <h1 onClick={() => this.props.history.goBack()} className='logo'>
+          <span>A</span>ppsus
+        </h1>
+        <nav>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/about'>About</NavLink>
+          <NavLink to='/mail'>Mail</NavLink>
+          <NavLink to='/keep'>Keep</NavLink>
+          <NavLink to='/book'>Book</NavLink>
+        </nav>
+      </section>
+    );
+  }
+}
+export const AppHeader = withRouter(_AppHeader);
