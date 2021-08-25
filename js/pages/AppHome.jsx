f20ba65d06982de61bb6c569f19dd312657fc108
bcd82 <barak.sidi@gmail.com>
2021-08-25 21:29:13 +0300

    fixed links for images(gh)

--- js/pages/AppHome.jsx
+++ js/pages/AppHome.jsx
@@ -5,9 +5,9 @@ export  function AppHome() {
         <section className="app-home main-layout">
             <h1>Welcome to AppSus</h1>
             <div className="app-row">
-                <AppPreview txt="Keep" imgUrl="../../assets/icons/home.png"/>
-                <AppPreview txt="Mail" imgUrl="../../assets/icons/home.png"/>
-                <AppPreview txt="Book" imgUrl="../../assets/icons/home.png"/>
+                <AppPreview txt="Keep" imgUrl="./assets/icons/home.png"/>
+                <AppPreview txt="Mail" imgUrl="./assets/icons/home.png"/>
+                <AppPreview txt="Book" imgUrl="./assets/icons/home.png"/>
             </div>
         </section>
     )
