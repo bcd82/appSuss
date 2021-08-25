f07b7e72ae8981fcfca467419c4408c707e6f0c7
bcd82 <barak.sidi@gmail.com>
2021-08-25 22:35:13 +0300

    changed side menu (mail)

--- js/apps/mail/cmps/MailMenu.jsx
+++ js/apps/mail/cmps/MailMenu.jsx
@@ -2,16 +2,15 @@ const { Link, NavLink, Route, Switch } = ReactRouterDOM;
 
 export function MailMenu() {
   return (
-    <div>
+    <nav className="mail-menu">
       <NavLink to="/mail/compose" exact>
-        <h1>Compose</h1>
+        <h1 className="compose-btn">Compose</h1>
       </NavLink>
-      <NavLink to="/mail/" exact>
-        List{" "}
-      </NavLink>
-      <NavLink to="/mail/234234" exact>
-        Details{" "}
-      </NavLink>{" "}
-    </div>
+      <ul>
+          <li>Inbox</li>
+          <li>Sent</li>
+          <li>Trash</li>
+      </ul>
+    </nav>
   );
 }
