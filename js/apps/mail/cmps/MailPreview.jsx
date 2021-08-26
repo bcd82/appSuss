9cd8f417f7ea9d13f3ace10c382d372cd7126c27
bcd82 <barak.sidi@gmail.com>
2021-08-26 14:02:39 +0300

    started reworking date display

--- js/apps/mail/cmps/MailPreview.jsx
+++ js/apps/mail/cmps/MailPreview.jsx
@@ -3,10 +3,10 @@ import { utilService } from "../../../services/util.service.js";
 export function MailPreview(props) {
   const { mail , onToggleStar , onClickMail} = props;
   
-  const getTimeToDisplay = ()=> {
-      let time = new Date(mail.sentAt);
-      return (time.getHours() + ':' +(time.getMinutes() < 10 ? '0'+ time.getMinutes() :time.getMinutes() ) + ' ' + time.getFullYear())
-  } 
+  // const getTimeToDisplay = ()=> {
+  //     let time = new Date(mail.sentAt);
+  //     return (time.getHours() + ':' +(time.getMinutes() < 10 ? '0'+ time.getMinutes() :time.getMinutes() ) + ' ' + time.getFullYear())
+  // } 
   
   return (
     <article className={`mail-preview ${ mail.isRead && 'read' }`} onClick={()=>onClickMail(mail.id)}>
@@ -20,7 +20,7 @@ export function MailPreview(props) {
       </p>
       <p className="subject">{mail.subject}</p>
       <p className="body-snippet">{mail.body.substring(0,40)}</p>
-      <p className="sent-time">{getTimeToDisplay()}</p>
+      <p className="sent-time">{utilService.getTimeToDisplay(mail.sentAt)}</p>
     </article>
   );
 }
