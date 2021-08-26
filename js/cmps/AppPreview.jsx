23cf6be3c3c79d4e73077ba2783320748c819110
bcd82 <barak.sidi@gmail.com>
2021-08-26 22:53:22 +0300

    started improving home page

--- js/cmps/AppPreview.jsx
+++ js/cmps/AppPreview.jsx
@@ -1,9 +1,12 @@
+const { Link } = ReactRouterDOM;
 
 export function AppPreview(props) {
     return (
         <div className="app-preview">
            <h2>{props.txt}</h2>
+           <p>Keep Notes of All of Your Favourite Stuff</p>
            <img src={props.imgUrl} />
+           <Link>Go to Keep App</Link>
         </div>
     )
 }
--- js/pages/AppHome.jsx
+++ js/pages/AppHome.jsx
@@ -1,11 +1,13 @@
 import {AppPreview} from '../cmps/AppPreview.jsx'
 
+
 export  function AppHome() {
     return (
         <section className="app-home main-layout">
             <h1>Welcome to AppSus</h1>
+            <h2>The #1 Keep/Mail/Book App !</h2>
             <div className="app-row">
-                <AppPreview txt="Keep" imgUrl="./assets/icons/home.png"/>
+                <AppPreview txt="Keep" imgUrl="./assets/imgs/cmps/sticky-notes.jpg"/>
                 <AppPreview txt="Mail" imgUrl="./assets/icons/home.png"/>
                 <AppPreview txt="Book" imgUrl="./assets/icons/home.png"/>
             </div>
