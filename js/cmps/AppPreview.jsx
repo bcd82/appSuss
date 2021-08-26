b6abd133475518ce748f12c7a6a8d2047b62ee65
bcd82 <barak.sidi@gmail.com>
2021-08-26 23:06:28 +0300

    countinued  improving of the  home page

--- js/cmps/AppPreview.jsx
+++ js/cmps/AppPreview.jsx
@@ -1,12 +1,13 @@
 const { Link } = ReactRouterDOM;
 
 export function AppPreview(props) {
+    const {title,txt,imgUrl} = props;
     return (
         <div className="app-preview">
-           <h2>{props.txt}</h2>
-           <p>Keep Notes of All of Your Favourite Stuff</p>
-           <img src={props.imgUrl} />
-           <Link>Go to Keep App</Link>
+           <h2>{title}</h2>
+           <p>{txt}</p>
+           <img src={imgUrl} />
+           <Link to={`/${title.toLowerCase()}/`}>Go to {title} App</Link>
         </div>
     )
 }
--- js/pages/AppHome.jsx
+++ js/pages/AppHome.jsx
@@ -7,9 +7,9 @@ export  function AppHome() {
             <h1>Welcome to AppSus</h1>
             <h2>The #1 Keep/Mail/Book App !</h2>
             <div className="app-row">
-                <AppPreview txt="Keep" imgUrl="./assets/imgs/cmps/sticky-notes.jpg"/>
-                <AppPreview txt="Mail" imgUrl="./assets/icons/home.png"/>
-                <AppPreview txt="Book" imgUrl="./assets/icons/home.png"/>
+                <AppPreview title='Keep' txt="Keep Notes of All of Your Favourite Stuff" imgUrl="./assets/imgs/cmps/sticky-notes.jpg"/>
+                <AppPreview title='Mail' txt="Keep in Touch With Friends and Enemies" imgUrl="./assets/imgs/cmps/mailbox.jpg"/>
+                <AppPreview title='Book' txt="" imgUrl="./assets/icons/home.png"/>
             </div>
         </section>
     )
