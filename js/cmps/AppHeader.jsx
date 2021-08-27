6aa60fbc422503fd593880b5a68daebcd6425e6b
bcd82 <barak.sidi@gmail.com>
2021-08-27 23:28:27 +0300

    mobile app menu sort of done

--- js/cmps/AppHeader.jsx
+++ js/cmps/AppHeader.jsx
@@ -4,7 +4,6 @@ import { AppMenu } from "./AppMenu.jsx";
 const _AppHeader = (props) => {
   return (
     <section className="main-layout app-header">
-      <div></div>
       <img src="./assets/imgs/cmps/horse.png"></img>
       <h1 onClick={() => props.history.push("/")} className="logo">
         <span>A</span>ppsus
--- js/cmps/AppMenu.jsx
+++ js/cmps/AppMenu.jsx
@@ -1,19 +1,37 @@
 const { NavLink } = ReactRouterDOM;
 
 export class AppMenu extends React.Component {
-    state = { 
-        isMobileMenuOpen : false
-    }
+  state = {
+    isMobileMenuOpen: false,
+  };
+
+  toggleAppMenu = (isClosing) => {
+    if (isClosing) this.setState({ isMobileMenuOpen: false });
+    this.setState({ isMobileMenuOpen: !this.state.isMobileMenuOpen });
+  };
+
   render() {
+    const { isMobileMenuOpen } = this.state;
     return (
-      <div>
-        <img src="/assets/imgs/cmps/apps.png"></img>
-        <nav className="app-links">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/mail?filter=inbox">Mail</NavLink>
-          <NavLink to="/keep">Keep</NavLink>
-          <NavLink to="/book">Book</NavLink>
+      <div className="app-menu">
+        <img
+          className="apps-icon"
+          src="./assets/imgs/cmps/apps.png"
+          onClick={this.toggleAppMenu}
+        />
+        <nav
+          className={`app-links ${isMobileMenuOpen ? "menu-open" : ""}`}
+          ref={this.createRef}
+        >
+          <NavLink to="/mail" className="app-link" onClick={()=>this.toggleAppMenu(true)}>
+            <img src="./assets/imgs/cmps/mail-app.png" />
+          </NavLink>
+          <NavLink to="/keep" className="app-link" onClick={()=>this.toggleAppMenu(true)}>
+            <img src="./assets/imgs/cmps/keep-app.png" />
+          </NavLink>
+          <NavLink to="/book" className="app-link" onClick={()=>this.toggleAppMenu(true)}>
+            <img src="./assets/imgs/cmps/book-app.png" />
+          </NavLink>
         </nav>
       </div>
     );
