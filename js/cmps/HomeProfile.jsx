04de46bb62c6e934505d27ecdc76b1d7f104739e
bcd82 <barak.sidi@gmail.com>
2021-08-28 13:46:55 +0300

    added about profiles

--- js/cmps/HomeProfile.jsx
+++ js/cmps/HomeProfile.jsx
@@ -2,16 +2,28 @@ export function HomeProfile(props) {
   return (
     <div className="home-profile">
       <div className="profile-details">
-        <img src={props.imgUrl} />
+        <img className="profile-img" src={props.imgUrl} />
         <h2>{props.name}</h2>
         <p>{props.title}</p>
+        <div className="links">
+          <a href={props.fbUrl} targert="blank">
+            <img src="./assets/imgs/cmps/fb.png" />
+          </a>
+          <a href={props.ghUrl} targert="blank">
+            <img src="./assets/imgs/cmps/gh.png" />
+          </a>
+          <a href={props.liUrl} targert="blank">
+            <img src="./assets/imgs/cmps/li.png" />
+          </a>
+        </div>
       </div>
       <div className="profile-text">
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
-          et officiis aut reiciendis alias rerum nemo qui dolores debitis error,
-          ipsam quasi, unde, officia enim repudiandae sed explicabo assumenda
-          corrupti quidem! Maxime error veritatis quisquam.
+          <i>
+            " Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Voluptatum et officiis aut reiciendis alias rerum nemo qui dolores
+            debitis error, ipsam quasi, unde, officia enim."
+          </i>
         </p>
       </div>
     </div>
