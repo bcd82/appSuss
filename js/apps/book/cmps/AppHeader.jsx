eb8325b22eaad9a410f119e9ff6972409209135a
bcd82 <barak.sidi@gmail.com>
2021-08-28 14:39:50 +0300

    fixed some css bugs

--- js/apps/book/cmps/AppHeader.jsx
+++ js/apps/book/cmps/AppHeader.jsx
@@ -1,37 +1,70 @@
 const { NavLink, withRouter } = ReactRouterDOM;
 
- class _AppHeader  extends React.Component {
-
+class _AppHeader extends React.Component {
   state = {
-    isMobileMenuOpen : false
-  }
+    isMobileMenuOpen: false,
+  };
   
-  toggleMenu = (ev) =>{
-    if(!this.state.isMobileMenuOpen) document.body.style.overflow = 'hidden';
-    else document.body.style.overflow = 'unset'
-    this.setState({isMobileMenuOpen:!this.state.isMobileMenuOpen})
+  componentWillUnmount() {
+    document.body.style.overflow = "unset";
   }
+
+  toggleMenu = (ev) => {
+    if (!this.state.isMobileMenuOpen) document.body.style.overflow = "hidden";
+    else document.body.style.overflow = "unset";
+    this.setState({ isMobileMenuOpen: !this.state.isMobileMenuOpen });
+  };
   render() {
-    const {isMobileMenuOpen} = this.state
+    const { isMobileMenuOpen } = this.state;
     return (
       <div>
-      <header className="book-header">
-        <div className="main-header main-layout">
-        <h1 className="logo" onClick={()=>{
-          this.props.history.push('/book/home/')
-        }}>BookSus</h1>
-        <nav className={isMobileMenuOpen ? 'mobile': ''}>
-          <NavLink to='/book/home' exact onClick={isMobileMenuOpen && this.toggleMenu}>Home</NavLink>
-          <NavLink to="/book/about" onClick={isMobileMenuOpen && this.toggleMenu}>About</NavLink>
-          <NavLink to="/book" exact onClick={isMobileMenuOpen && this.toggleMenu}>Books</NavLink>
-          <NavLink to="/book/add" exact onClick={isMobileMenuOpen && this.toggleMenu}>Add Book</NavLink>
-         </nav>
-          <button className="menu-btn-mobile" onClick={this.toggleMenu}><img src="./assets/imgs/cmps/hamburger.png"/></button>
-        </div>
-      </header>
+        <header className="book-header">
+          <div className="main-header main-layout">
+            <h1
+              className="logo"
+              onClick={() => {
+                this.props.history.push("/book/home/");
+              }}
+            >
+              BookSus
+            </h1>
+            <nav className={isMobileMenuOpen ? "mobile" : ""}>
+              <NavLink
+                to="/book/home"
+                exact
+                onClick={isMobileMenuOpen && this.toggleMenu}
+              >
+                Home
+              </NavLink>
+              <NavLink
+                to="/book/about"
+                onClick={isMobileMenuOpen && this.toggleMenu}
+              >
+                About
+              </NavLink>
+              <NavLink
+                to="/book"
+                exact
+                onClick={isMobileMenuOpen && this.toggleMenu}
+              >
+                Books
+              </NavLink>
+              <NavLink
+                to="/book/add"
+                exact
+                onClick={isMobileMenuOpen && this.toggleMenu}
+              >
+                Add Book
+              </NavLink>
+            </nav>
+            <button className="menu-btn-mobile" onClick={this.toggleMenu}>
+              <img src="./assets/imgs/cmps/hamburger.png" />
+            </button>
+          </div>
+        </header>
       </div>
     );
-  };
   }
+}
 
 export const AppHeader = withRouter(_AppHeader);
