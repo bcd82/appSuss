b084a817c0047e2b3d9e10b99bbd16099258eae9
bcd82 <barak.sidi@gmail.com>
2021-08-28 14:01:34 +0300

    fixed header bug

--- js/cmps/AppHeader.jsx
+++ js/cmps/AppHeader.jsx
@@ -4,10 +4,11 @@ import { AppMenu } from "./AppMenu.jsx";
 const _AppHeader = (props) => {
   return (
     <section className="main-layout app-header">
-      <img src="./assets/imgs/cmps/horse.png"></img>
+      <img src="./assets/imgs/cmps/horse.png"  onClick={() => props.history.push("/")}/>
       <h1 onClick={() => props.history.push("/")} className="logo">
         <span>A</span>ppsus
       </h1>
+      <div className="spacer"></div>
       <AppMenu />
     </section>
   );
